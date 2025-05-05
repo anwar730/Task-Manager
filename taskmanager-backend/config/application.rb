c9ecf9a1da6248339e03b4ef41e2ed488dde3902
anwarismail <anwaarismail449@gmail.com>
2025-05-05 21:08:53 +0300

    Update cors file

--- taskmanager-backend/config/application.rb
+++ taskmanager-backend/config/application.rb
@@ -22,8 +22,7 @@ module TaskmanagerBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
+    
 # Be sure to restart your server when you modify this file.
 
 # Avoid CORS issues when API is called from the frontend app.
@@ -44,11 +43,12 @@ module TaskmanagerBackend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.session_store :cookie_store, key: 'TaskmanagerBackend'
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
--- taskmanager-backend/config/initializers/cors.rb
+++ taskmanager-backend/config/initializers/cors.rb
@@ -1,12 +1,9 @@
-# config/initializers/cors.rb
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173' # <--- your Vite frontend
-
+    origins 'http://localhost:5173/' # Or 'localhost:3000' for development
     resource '*',
       headers: :any,
-      methods: [:get, :post, :patch, :put, :delete, :options],
-      credentials: true # <--- very important
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
