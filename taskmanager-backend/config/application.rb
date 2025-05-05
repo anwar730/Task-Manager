ace5d102e7b2005121db6b30979689106304d513
anwarismail <anwaarismail449@gmail.com>
2025-05-05 18:23:03 +0300

    Update cookie settings

--- taskmanager-backend/config/application.rb
+++ taskmanager-backend/config/application.rb
@@ -55,7 +55,7 @@ end
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.session_store :cookie_store, key: 'TasklisterApi'
+    config.session_store :cookie_store, key: 'TaskmanagerBackend'
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
