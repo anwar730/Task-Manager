7b2429986efd81e6161b32e1db7530e773aa53bc
anwarismail <anwaarismail449@gmail.com>
2025-05-06 16:58:11 +0300

    update frontend link

--- taskmanager-backend/config/initializers/cors.rb
+++ taskmanager-backend/config/initializers/cors.rb
@@ -28,8 +28,8 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://task-manager-alpha-gilt.vercel.app/', 'http://localhost:5173'
-    
+    origins 'https://task-manager-alpha-gilt.vercel.app', 'http://localhost:5173'
+
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
